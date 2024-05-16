a3b411134cce0b38626c90a8eb24e675053fbb7d
shc <sh@c>
2024-05-16 20:21:08 +0200

    refactor(wallpaper): stow to xdg/*

--- setup.sh
+++ setup.sh
@@ -3,44 +3,49 @@
 SCRIPT_PATH="$(realpath "$(dirname "${0}")")"
 cd "${SCRIPT_PATH}" || exit 3
 
-make_wallpaper() {
-    local _wallpapers_path="${SCRIPT_PATH}/.config/sway/conf/general/components/output/wallpapers"
-    local linkname="current"
-    local _select=""
-    (
-        cd "${_wallpapers_path}" || exit 3
-
-        case "${1}" in
-            "select")
-                printf "\n"
-                printf "select wallpaper: "
-                printf "\n"
-                _current="$(find "." -type f | fzf --reverse --height=37%)"
-                _select="yes"
-                ;;
-            *)
-                _current="Leopard_Server.jpg"
-                ;;
-        esac
-
-        if [ "${_select}" ] || ! [ -e "${linkname}" ]; then
-            ln -s -f "${_current}" "current"
-        fi
-    )
-}
+WALLPAPER_PATH="${HOME}/xdg/MDA/Pic/wallpapers"
 
 __stow() {
+    mkdir -p "${WALLPAPER_PATH}"
+
     (
         cd .. && stow -R "$(basename "${SCRIPT_PATH}")"
     )
 }
 
-main() {
-    make_wallpaper "${@}"
-    __stow
+__wallpaper() {
+    local _choice=""
+    local _need_update=""
+    (
+        cd "${WALLPAPER_PATH}" || exit 3
+        case "${1}" in
+            "select")
+                printf "\n"
+                printf "select wallpaper: "
+                printf "\n"
+                _choice="$(find "." -type f | fzf --reverse --height=37%)"
+                _need_update="yes"
+                ;;
+            *)
+                _choice="Leopard_Server.jpg"
+                ;;
+        esac
+        _choice="$(realpath "${_choice}")"
 
-    unset SCRIPT_PATH
-    unset -f make_wallpaper __stow
+        cd "${SCRIPT_PATH}/.config/sway/conf/components/adhoc/" || exit 3
+        local _link="wallpaper"
+        if [ "${_need_update}" ] || ! [ -e "${_link}" ]; then
+            ln -s -f "${_choice}" "${_link}"
+        fi
+    )
+}
+
+main() {
+    __stow
+    __wallpaper "${@}"
+
+    unset SCRIPT_PATH WALLPAPER_PATH
+    unset -f __stow __wallpaper
 }
 main "${@}"
 unset -f main
