bccd956d7e2301ec652c1139902155292a740fc7
shc <sh@c>
2024-05-26 01:08:20 +0200

    feat(river): 2nd batch config cleanup

--- .config/river/default.sh
+++ .config/river/default.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env dash
 
 __base() {
-    riverctl map normal Super Return spawn foot
-
-    # Super+Q to close the focused view
+    riverctl map normal Super Return spawn \
+        "foot --config ${HOME}/.config/foot/foot_alpha.ini"
     riverctl map normal Super Q close
-
-    # Super+Shift+E to exit river
     riverctl map normal Super+Shift E exit
 }
 __base
@@ -21,12 +18,12 @@ __navigate() {
     riverctl map normal Super+Shift K swap previous
 
     # focus, output
-    riverctl map normal Super Period focus-output next
-    riverctl map normal Super Comma focus-output previous
+    riverctl map normal Super Left focus-output next
+    riverctl map normal Super Right focus-output previous
 
     # Super+Shift+{Period,Comma} to send the focused view to the next/previous output
-    riverctl map normal Super+Shift Period send-to-output next
-    riverctl map normal Super+Shift Comma send-to-output previous
+    riverctl map normal Super+Shift Left send-to-output next
+    riverctl map normal Super+Shift Right send-to-output previous
 }
 __navigate
 
@@ -34,73 +31,69 @@ __layout() {
     # Super+Return to bump the focused view to the top of the layout stack
     riverctl map normal Super+Shift Return zoom
 
-    riverctl map normal Super+Shift H send-layout-cmd rivertile "main-ratio -0.05"
-    riverctl map normal Super+Shift L send-layout-cmd rivertile "main-ratio +0.05"
+    # +0.05
+    riverctl map normal Super+Alt H send-layout-cmd rivertile "main-ratio 0.33"
+    riverctl map normal Super+Alt L send-layout-cmd rivertile "main-ratio 0.67"
 
-    # riverctl map normal Super+Shift H send-layout-cmd rivertile "main-count +1"
-    # riverctl map normal Super+Shift L send-layout-cmd rivertile "main-count -1"
+    riverctl map normal Super+Alt K send-layout-cmd rivertile "main-count -1"
+    riverctl map normal Super+Alt J send-layout-cmd rivertile "main-count +1"
 
-    # Super+{Up,Right,Down,Left} to change layout orientation
-    riverctl map normal Super Up send-layout-cmd rivertile "main-location top"
-    riverctl map normal Super Right send-layout-cmd rivertile "main-location right"
-    riverctl map normal Super Down send-layout-cmd rivertile "main-location bottom"
-    riverctl map normal Super Left send-layout-cmd rivertile "main-location left"
+    riverctl map normal Super+Alt Up send-layout-cmd rivertile "main-location top"
+    riverctl map normal Super+Alt Right send-layout-cmd rivertile "main-location right"
+    riverctl map normal Super+Alt Down send-layout-cmd rivertile "main-location bottom"
+    riverctl map normal Super+Alt Left send-layout-cmd rivertile "main-location left"
 }
 __layout
 
-__view() {
-    # Super+Alt+{H,J,K,L} to move views
-    riverctl map normal Super+Alt H move left 100
-    riverctl map normal Super+Alt J move down 100
-    riverctl map normal Super+Alt K move up 100
-    riverctl map normal Super+Alt L move right 100
+__float() {
+    # riverctl map normal Super+Alt H move left 100
+    # riverctl map normal Super+Alt J move down 100
+    # riverctl map normal Super+Alt K move up 100
+    # riverctl map normal Super+Alt L move right 100
 
-    # Super+Alt+Control+{H,J,K,L} to snap views to screen edges
-    riverctl map normal Super+Alt+Control H snap left
-    riverctl map normal Super+Alt+Control J snap down
-    riverctl map normal Super+Alt+Control K snap up
-    riverctl map normal Super+Alt+Control L snap right
+    # # snap edge of view to edge of screen
+    # riverctl map normal Super+Alt+Control H snap left
+    # riverctl map normal Super+Alt+Control J snap down
+    # riverctl map normal Super+Alt+Control K snap up
+    # riverctl map normal Super+Alt+Control L snap right
 
-    # Super+Alt+Shift+{H,J,K,L} to resize views
-    riverctl map normal Super+Alt+Shift H resize horizontal -100
-    riverctl map normal Super+Alt+Shift J resize vertical 100
-    riverctl map normal Super+Alt+Shift K resize vertical -100
-    riverctl map normal Super+Alt+Shift L resize horizontal 100
+    # riverctl map normal Super+Alt+Shift H resize horizontal -100
+    # riverctl map normal Super+Alt+Shift J resize vertical 100
+    # riverctl map normal Super+Alt+Shift K resize vertical -100
+    # riverctl map normal Super+Alt+Shift L resize horizontal 100
 
-    # Super + Left Mouse Button to move views
     riverctl map-pointer normal Super BTN_LEFT move-view
-
-    # Super + Right Mouse Button to resize views
     riverctl map-pointer normal Super BTN_RIGHT resize-view
-
-    # Super + Middle Mouse Button to toggle float
     riverctl map-pointer normal Super BTN_MIDDLE toggle-float
 }
-__view
+__float
 
 __tags() {
-    local tags
+    local _tag
     for i in $(seq 1 9); do
-        tags="$((1 << (i - 1)))"
+        _tag="$((1 << (i - 1)))"
 
-        # tags to show
-        riverctl map normal Super "${i}" set-focused-tags "${tags}"
+        # reset: show this tag only
+        riverctl map normal Super "${i}" set-focused-tags "${_tag}"
 
-        # Super+Shift+[1-9] to tag focused view with tag [0-8]
-        riverctl map normal Super+Shift "${i}" set-view-tags "${tags}"
+        # toggle visibility of this tag
+        riverctl map normal Super+Control "${i}" toggle-focused-tags "${_tag}"
 
-        # Super+Control+[1-9] to toggle focus of tag [0-8]
-        riverctl map normal Super+Control "${i}" toggle-focused-tags "${tags}"
+        # for view: reset to this tag only
+        riverctl map normal Super+Shift "${i}" set-view-tags "${_tag}"
 
-        # Super+Shift+Control+[1-9] to toggle tag [0-8] of focused view
-        riverctl map normal Super+Shift+Control "${i}" toggle-view-tags "${tags}"
+        # for view: toggle this tag
+        riverctl map normal Super+Shift+Control "${i}" toggle-view-tags "${_tag}"
     done
 
-    # Super+0 to focus all tags
-    # Super+Shift+0 to tag focused view with all tags
-    local all_tags="$(((1 << 32) - 1))"
-    riverctl map normal Super 0 set-focused-tags "${all_tags}"
-    riverctl map normal Super+Shift 0 set-view-tags "${all_tags}"
+    local _all_tags="$(((1 << 32) - 1))"
+    # show all tags
+    riverctl map normal Super 0 set-focused-tags "${_all_tags}"
+    # for view: enable all tags
+    riverctl map normal Super+Shift 0 set-view-tags "${_all_tags}"
+
+    riverctl map normal Super Tab focus-previous-tags
+    riverctl map normal Super+Shift Tab send-to-previous-tags
 }
 __tags
 
@@ -112,41 +105,46 @@ __passthrough() {
 __passthrough
 
 __misc() {
-    riverctl map normal Super F toggle-float
+    riverctl map normal Super G toggle-float
     riverctl map normal Super grave toggle-fullscreen
 
-    local mode
-    # Various media key mapping examples for both normal and locked mode which do
-    # not have a modifier
-    for mode in "normal" "locked"; do
-        # Eject the optical drive (well if you still have one that is)
-        riverctl map "${mode}" None XF86Eject spawn 'eject -T'
+    # local mode
+    # # Various media key mapping examples for both normal and locked mode which do
+    # # not have a modifier
+    # for mode in "normal" "locked"; do
+    #     # Eject the optical drive (well if you still have one that is)
+    #     riverctl map "${mode}" None XF86Eject spawn 'eject -T'
 
-        # Control pulse audio volume with pamixer (https://github.com/cdemoulins/pamixer)
-        riverctl map "${mode}" None XF86AudioRaiseVolume spawn 'pamixer -i 5'
-        riverctl map "${mode}" None XF86AudioLowerVolume spawn 'pamixer -d 5'
-        riverctl map "${mode}" None XF86AudioMute spawn 'pamixer --toggle-mute'
+    #     # Control pulse audio volume with pamixer (https://github.com/cdemoulins/pamixer)
+    #     riverctl map "${mode}" None XF86AudioRaiseVolume spawn 'pamixer -i 5'
+    #     riverctl map "${mode}" None XF86AudioLowerVolume spawn 'pamixer -d 5'
+    #     riverctl map "${mode}" None XF86AudioMute spawn 'pamixer --toggle-mute'
 
-        # Control MPRIS aware media players with playerctl (https://github.com/altdesktop/playerctl)
-        riverctl map "${mode}" None XF86AudioMedia spawn 'playerctl play-pause'
-        riverctl map "${mode}" None XF86AudioPlay spawn 'playerctl play-pause'
-        riverctl map "${mode}" None XF86AudioPrev spawn 'playerctl previous'
-        riverctl map "${mode}" None XF86AudioNext spawn 'playerctl next'
+    #     # Control MPRIS aware media players with playerctl (https://github.com/altdesktop/playerctl)
+    #     riverctl map "${mode}" None XF86AudioMedia spawn 'playerctl play-pause'
+    #     riverctl map "${mode}" None XF86AudioPlay spawn 'playerctl play-pause'
+    #     riverctl map "${mode}" None XF86AudioPrev spawn 'playerctl previous'
+    #     riverctl map "${mode}" None XF86AudioNext spawn 'playerctl next'
 
-        # Control screen backlight brightness with brightnessctl (https://github.com/Hummer12007/brightnessctl)
-        riverctl map "${mode}" None XF86MonBrightnessUp spawn 'brightnessctl set +5%'
-        riverctl map "${mode}"None XF86MonBrightnessDown spawn 'brightnessctl set 5%-'
-    done
+    #     # Control screen backlight brightness with brightnessctl (https://github.com/Hummer12007/brightnessctl)
+    #     riverctl map "${mode}" None XF86MonBrightnessUp spawn 'brightnessctl set +5%'
+    #     riverctl map "${mode}"None XF86MonBrightnessDown spawn 'brightnessctl set 5%-'
+    # done
 }
+__misc
 
 __visual() {
-    local COLOR_GREY_DARK="2b272f" COLOR_GREY_BRIGHT="97879f" COLOR_WHITE="efe3fb"
-    riverctl background-color "0x${COLOR_GREY_DARK}7f"
+    local COLOR_BLACK="000000"
+    local COLOR_GREY_DARK="2b272f"
+    local COLOR_GREY_BRIGHT="97879f"
+    local COLOR_WHITE="efe3fb"
 
-    riverctl border-width 2
-    riverctl border-color-urgent "0x${COLOR_WHITE}7f"
+    riverctl background-color "0x${COLOR_GREY_DARK}"
+
+    riverctl border-width 3
+    riverctl border-color-unfocused "0x${COLOR_BLACK}e7"
     riverctl border-color-focused "0x${COLOR_GREY_BRIGHT}"
-    riverctl border-color-unfocused "0x${COLOR_GREY_DARK}7f"
+    riverctl border-color-urgent "0x${COLOR_WHITE}"
 
     riverctl xcursor-theme capitaine-cursors 24
     riverctl hide-cursor when-typing enabled
@@ -154,11 +152,33 @@ __visual() {
 }
 __visual
 
-# Set keyboard repeat rate
-riverctl set-repeat 50 300
+__rule() {
+    riverctl rule-add ssd
 
-# Make all views with an app-id that starts with "float" and title "foo" start floating.
-riverctl rule-add -app-id 'float*' -title 'foo' float
+    # Make all views with an app-id that starts with "float" and title "foo" start floating.
+    riverctl rule-add -app-id 'float*' -title 'foo' float
+}
+__rule
 
-# Make all views with app-id "bar" and any title use client-side decorations
-riverctl rule-add -app-id "bar" csd
+__input() {
+    # $ riverctl list-inputs
+    # $ riverctl list-input-configs
+
+    riverctl keyboard-layout \
+        -options "caps:escape,grp:win_space_toggle,lv2:lsgt_switch" \
+        "us,ch(fr),ru(phonetic)"
+    # keyboard repeat-rate
+    riverctl set-repeat 50 300
+
+    riverctl input "pointer-*" accel-profile flat
+    riverctl input "pointer-*" pointer-accel 2.0
+    riverctl input "pointer-*" scroll-factor 1.0
+    riverctl input "pointer-*" middle-emulation disabled
+    riverctl input "pointer-*" disable-while-typing enabled
+    riverctl input "pointer-*" disable-while-trackpointing enabled
+    riverctl input "pointer-*" tap enabled
+
+    local _touchpad="pointer-1739-0-Synaptics_TM3625-010"
+    riverctl input "${_touchpad}" natural-scroll enabled
+}
+__input
--- .config/river/layout.sh
+++ .config/river/layout.sh
@@ -5,4 +5,4 @@
 riverctl default-layout rivertile
 __cmd_layout main-count 1
 __cmd_layout main-location left
-__cmd_layout main-ratio 0.37
+__cmd_layout main-ratio 0.67
