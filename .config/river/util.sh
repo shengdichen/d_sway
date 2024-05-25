05b36550ad7848e09b9e769e24b92fd044b6f715
shc <sh@c>
2024-05-26 01:12:38 +0200

    feat(river): utils

--- .config/river/util.sh
+++ .config/river/util.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env dash
 
+__nohup() {
+    nohup "${@}" 1>/dev/null 2>&1 &
+}
+
+__kill() {
+    pkill -f "${@}"
+}
+
 __cmd_layout() {
     riverctl send-layout-cmd rivertile "${*}"
 }
