1221b6f29a8fbfd25680092962cab69e0573e2f5
shc <sh@c>
2024-06-27 04:31:21 +0200

    chore(hypr): remove legacy bash scripts

--- .local/bin/hypr.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env dash
-
-# shellcheck disable=SC1091
-. "${HOME}/.local/bin/hyprutil.sh"
-
-__run "${HOME}/.config/fuzzel/hypr.sh"
--- .local/bin/hyprutil.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-
-__run_float() {
-    hyprctl dispatch -- exec "[float]" footclient --title "throwaway" -- "${@}"
-}
-
-__run() {
-    hyprctl dispatch -- exec footclient --title "throwaway" -- "${@}"
-}
